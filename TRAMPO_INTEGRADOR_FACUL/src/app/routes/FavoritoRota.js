221212705b99518d5cc6e5bdc57269daccd9fa00
Arthur Bitencourt <113964510+Thurvabitt@users.noreply.github.com>
2023-06-20 15:12:39 -0300

    Update FavoritoRota.js

--- TRAMPO_INTEGRADOR_FACUL/src/app/routes/FavoritoRota.js
+++ TRAMPO_INTEGRADOR_FACUL/src/app/routes/FavoritoRota.js
@@ -1,22 +1,20 @@
-// Importando o framework Express e o controlador de usuário
 const express = require('express')
 const FavoritoController = require('../controllers/FavoritoController')
 const FavRouter = express.Router()
 
-//Adicionar Usuario
+//Add
 FavRouter.post('/adicionarFavorito', (req, res) => {FavoritoController.store(req, res); console.log('eu chupo pito')});
 
-//listar usuarios
+//list
 FavRouter.get('/listarFavorito', FavoritoController.index);
 
-//buscar usuario por ID
+//search
 FavRouter.get('/buscarPorId/:id', FavoritoController.show);
 
-//atualizar Usuario
+//att
 FavRouter.put('/atualizarFavorito/:id', FavoritoController.update);
 
-//deletar usuario por ID
+//del
 FavRouter.delete('/deletarFavorito/:id', FavoritoController.deleteFavorito);
 
-// Exporta as rotas
 module.exports = FavRouter
