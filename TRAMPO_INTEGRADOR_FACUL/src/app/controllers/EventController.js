03589d2cec13141f8e62b7f6f352d4af31271765
Arthur Bitencourt <113964510+Thurvabitt@users.noreply.github.com>
2023-06-20 15:11:38 -0300

    Update EventController.js

--- TRAMPO_INTEGRADOR_FACUL/src/app/controllers/EventController.js
+++ TRAMPO_INTEGRADOR_FACUL/src/app/controllers/EventController.js
@@ -1,35 +1,28 @@
 const express = require('express')
 const EventRepository = require('../models/EventRepository')
 
-
-
-
-    // criar favorito
-    const store = async(req, res) => {                            // Recebe a requisição (req) e a resposta (res)
+    const store = async(req, res) => {                         
         try{
-        const events = req.body;                                 // Extrai o objeto do usuário do corpo da requisição (req.body)
-        const row = await EventRepository.create(events);      // Chama a função create do UsuarioRepository para criar o usuário no banco de dados
+        const events = req.body;                                
+        const row = await EventRepository.create(events);      
         res.json(events);    
-        }                                        // Retorna a resposta como um objeto JSON contendo os dados do usuário criado
+        }                                       
         catch (error) {
             res.json(500)
         }    
     }
 
-    // listar favorito
     const index = async (req, res) =>{
         const row = await EventRepository.findAll();
         res.json(row);
     }
 
-    // buscar favorito por id
     const show = async(req, res) => {
         const id = req.params.id;
         const row = await EventRepository.findById(id);
         res.json(row);
     }
 
-    // atualizar favorito
     const update = async(req, res) =>{
         const events = req.body;
         const id = req.params.id;
@@ -37,7 +30,6 @@ const EventRepository = require('../models/EventRepository')
         res.json(row);
     }
 
-    // deletar favorito
     const deleteevents = async(req, res) => {
         const id = req.params.id;
         const row = await EventRepository.deleteevents(id);
